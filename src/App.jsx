4ff92f65b45d31ace345441ff8291d4970d321f2
dropperDEV <jamescosta4747@Gmail.com>
2023-12-17 22:39:01 -0300

    feat: UserInfo made

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import Home from './pages/Home'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import { Home } from './pages/home'
+import { Login } from './pages/login'
+import { Feed } from "./pages/feed"
+import { Register } from "./pages/register"
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/login" element={<Login/>} />
-        </Routes>
-      </Router>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/Feed" element={<Feed/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
